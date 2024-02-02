a4fd4846c12393e82108453f6c000a7edc94fff4
Daniel Campos <56805268+retrohell@users.noreply.github.com>
2024-02-02 15:03:11 -0600

    first changes

--- djapp/views.py
+++ djapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import ExampleModel
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the djapp index.")
+# Path: djexample/djapp/urls.py
+
+def exampleModel(request):
+    example = get_object_or_404(ExampleModel.objects.values())
+    return JsonResponse(example, safe=False)
+
+def exampleModelById(request, id):
+    example = get_object_or_404(ExampleModel, id=id)
+    return HttpResponse('Example: %s' % example.name)
